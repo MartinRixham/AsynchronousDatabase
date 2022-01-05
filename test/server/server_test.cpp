65505f0bb635a23692310991452bbd77ce6fb2ff
Martin Rixham <martin.rixham@gmail.com>
2022-01-05 19:37:33 +0000

    Test get request.

---
+++ test/server/server_test.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include <curl/curl.h>
+
+#include <memory>
+
+#include "server/server.h"
+
+TEST(Server, get_request)
+{
+    auto srv = std::make_shared<server>("127.0.0.1", 0);
+    auto port = srv->port();
+
+    std::thread([srv]()
+        {
+            srv->serve();
+        });
+
+    EXPECT_EQ(1, 2);
+
+    auto curl = curl_easy_init();
+    std::string response;
+
+    curl_easy_setopt(curl, CURLOPT_URL, "localhost:" + std::to_string(port));
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+
+    EXPECT_EQ(response, "Hello, world!");
+}
--- test/server_test.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(Server, get_request)
-{
-    EXPECT_EQ(true, true);
-}
