3ecbabec4ad45741864b0620c1e19f1f41bc4077
Martin Rixham <martin.rixham@gmail.com>
2026-09-09 23:59:55 +0100

    Improve logging.

--- src/log.h
+++ src/log.h
@@ -2,11 +2,15 @@
 #define LOG_H
 
 #include <iostream>
+#include <syncstream>
 
 #define LEVEL_DEBUG 1
 
 #if LOG == LEVEL_DEBUG
-#define DEBUG(message) std::cerr << "DEBUG: " << message << std::endl;
+// Every thread of the pool logs to the one stream, and a line written in several inserts is one
+// another thread splices itself into the middle of. The line is assembled here and handed over
+// whole.
+#define DEBUG(message) std::osyncstream(std::cerr) << "DEBUG: " << message << std::endl;
 #else
 #define DEBUG(message)
 #endif
